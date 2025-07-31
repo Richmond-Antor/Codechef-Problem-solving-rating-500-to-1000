ce93879e3ab502e56da11330946fd9d7ab71e8db
Richmond Antor <antorgm2@gmail.com>
2025-07-31 23:36:31 +0600

    Create Jenga Night.cpp

---
+++ Jenga Night.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        if (x % n == 0)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}

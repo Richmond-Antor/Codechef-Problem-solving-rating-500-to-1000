52dea85b70967c40def0e6eab6738f8a9fa40499
Richmond Antor <antorgm2@gmail.com>
2025-08-06 23:34:11 +0600

    Create Watching Movies at 2x.cpp

---
+++ Watching Movies at 2x.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x,y;
+	
+	    
+	    std::cin >> x>>y;
+	    
+	   int a=x-y;
+	   int b=y/2;
+	    std::cout << a+b << std::endl;
+	    
+	    
+	    
+	}
+
+
